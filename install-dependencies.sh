1d7500780c614ae7e870346ce9c68543291c52d6
Travis <dev+travis@dotmpe.com>
2018-02-18 04:51:20 +0100

    Travis CI

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,51 +2,9 @@
 
 set -e
 
-stderr()
-{
-  echo "$log_pref$1" >&2
-  test -z "$2" || exit $2
-}
-
-test -z "$Build_Debug" || set -x
-
-test -z "$Build_Deps_Default_Paths" || {
-
-  test -n "$SRC_PREFIX" || {
-    test -w /src/ \
-      && SRC_PREFIX=/src/ \
-      || SRC_PREFIX=$HOME/build
-  }
-
-  test -n "$PREFIX" || {
-    test -w /usr/local/ \
-      && PREFIX=/usr/local/ \
-      || PREFIX=$HOME/.local
-  }
-
-  stderr "Setting default paths: SRC_PREFIX=$SRC_PREFIX PREFIX=$PREFIX"
-}
-
-test -n "$sudo" || sudo=
-test -z "$sudo" || pref="sudo $pref"
-test -z "$dry_run" || pref="echo $pref"
-
-test -n "$SRC_PREFIX" ||
-  stderr "Not sure where to checkout (SRC_PREFIX missing)" 1
-
-test -n "$PREFIX" ||
-  stderr "Not sure where to install (PREFIX missing)" 1
+type stderr >/dev/null 2>&1 || . ./profile.sh
 
 
-echo SRC_PREFIX=$SRC_PREFIX
-echo PREFIX=$PREFIX
-echo "install-dependencies: '$*'"
-test -d $SRC_PREFIX || ${pref} mkdir -vp $SRC_PREFIX
-test -d $PREFIX || ${pref} mkdir -vp $PREFIX
-
-
-# Last version at this time
-# cweb-3.64c
 install_cweb()
 {
   test -e $SRC_PREFIX/github.com/ascherer/cweb || {
@@ -55,7 +13,6 @@ install_cweb()
   }
   (
     cd $SRC_PREFIX/github.com/ascherer/cweb
-    ${sudo}mkdir -p $PREFIX/bin
     test -n "$CWEB_VERSION" && {
       git checkout $CWEB_VERSION
       make
--- profile.sh
+++ profile.sh
@@ -1,11 +1,57 @@
-export PREFIX=/usr/local
-export SRC_PREFIX=/src
-export sudo='sudo '
 #export CWEB_VERSION=cweb-3.64c
 export CWEB_VERSION=cweb-3.43
+# Last version at this time
+# cweb-3.64c
 
-for bin in cweave ctangle
-do
-  rm $PREFIX/bin/$bin || true
-  ln -s $PREFIX/bin/$bin-$CWEB_VERSION $PREFIX/bin/$bin
-done
+#export sudo='sudo '
+
+stderr()
+{
+  echo "$log_pref$1" >&2
+  test -z "$2" || exit $2
+}
+
+test -z "$Build_Debug" || set -x
+
+test -z "$Build_Deps_Default_Paths" || {
+
+  test -n "$SRC_PREFIX" || {
+    test -w /src/ \
+      && SRC_PREFIX=/src/ \
+      || SRC_PREFIX=$HOME/build
+  }
+
+  test -n "$PREFIX" || {
+    test -w /usr/local/ \
+      && PREFIX=/usr/local/ \
+      || PREFIX=$HOME/.local
+  }
+
+  stderr "Setting default paths: SRC_PREFIX=$SRC_PREFIX PREFIX=$PREFIX"
+}
+
+test -n "$sudo" || sudo=
+test -z "$sudo" || pref="sudo $pref"
+test -z "$dry_run" || pref="echo $pref"
+
+test -n "$SRC_PREFIX" ||
+  stderr "Not sure where to checkout (SRC_PREFIX missing)" 1
+
+test -n "$PREFIX" ||
+  stderr "Not sure where to install (PREFIX missing)" 1
+
+
+echo SRC_PREFIX=$SRC_PREFIX
+echo PREFIX=$PREFIX
+echo "install-dependencies: '$*'"
+test -d $SRC_PREFIX || ${pref} mkdir -vp $SRC_PREFIX
+test -d $PREFIX || ${pref} mkdir -vp $PREFIX/bin
+
+
+test -z "$CWEB_VERSION" || {
+  for bin in cweave ctangle
+  do
+    rm $PREFIX/bin/$bin || true
+    ln -s $PREFIX/bin/$bin-$CWEB_VERSION $PREFIX/bin/$bin
+  done
+}
